631c10901a828ca71c146f0b27cb4b9e2d00eeaa
freakdran <ich.sag@web.de>
2018-04-20 16:52:07 +0200

    models

---
+++ Assets/Scenes/Zelt/move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	public Transform trans;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("GO");
+            do
+            {
+                Debug.Log("GO2");
+
+                trans.right += new Vector3(-0.1f,0,0);
+            } while (trans.position.x > -10);
+        }
+
+	}
+}
