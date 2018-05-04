480c28d253e380290b4e99a7b9db25a5e9920ab8
freakdran <ich.sag@web.de>
2018-05-04 10:06:20 +0200

    IP Geaendert

--- Assets/DATEN/Konstanten.cs
+++ Assets/DATEN/Konstanten.cs
@@ -5,6 +5,6 @@ using System;
 
 public class Konstanten
 {
-    public static string URL = "http://67ba6017.ngrok.io/imao/api/spiel/";
+    public static string URL = "localhost:8080/imao/api/spiel/";
     public static int POLLING = 100; // nach dieser Anzahl Frames erfolgt ein Update-Poll
 }
